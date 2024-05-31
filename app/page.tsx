921b8163b1198c5eb971120309f8a5d950af96fa
MedAmine1212 <khaili.amine@hotmail.fr>
2024-05-31 09:42:40 +0100

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+'use client'
 import Hero from "./(main)/0.Hero";
 import Cases from "./(main)/3.Cases";
 import GetStarted from "./(main)/8.GetStarted";
 import Skills from "~/app/(main)/1.Skills";
 import Methodologies from "~/app/(main)/Methodologies";
 import Services from "./(main)/Services";
-import { usePageView } from "./hooks/usePageView";
+import {useEffect} from "react";
+import * as gtag from "~/app/lib/gtag";
+import {usePathname} from "next/navigation";
 
 export default function Home() {
-    usePageView()
+
+    const pathname = usePathname()
+    useEffect(() => {
+        if (typeof window === 'undefined') {
+            return
+        }
+
+        const handleRouteChange = (url: string) => {
+            gtag.pageView(url)
+        }
+
+        handleRouteChange(pathname)
+    }, [pathname]);
       return (
         <main className="overflow-hidden">
             <Hero />
