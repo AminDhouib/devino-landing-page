53e538c35e7ee082742a189769f1ca03bd14036a
Alaeddin BENSALAH <alaeddinbs@gmail.com>
2024-11-27 19:55:41 +0100

    add custom title for each page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,16 +2,17 @@ import Hero from "./(pricing)/0.Hero";
 import Benefits from "./(pricing)/1.Benefits";
 import FreeTrial from "./(pricing)/2.FreeTrial";
 import Comparison from "./(pricing)/5.Comparison";
-import FAQ from "./(pricing)/6.FAQ";
 import GetStarted from "./(pricing)/7.GetStarted";
 import Pricing from "~/app/pricing/(pricing)/Pricing";
-import AppHeader from "~/app/AppHeader";
 import React from "react";
+
+export const metadata = {
+    title: "Devino | Pricing",
+};
 
 export default function Page() {
   return (
     <main>
-        <AppHeader/>
       <Hero />
       <Pricing />
       <Benefits />
